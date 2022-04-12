4bd132e577ff173356d4fd75b21b6e7717a22490
David Johnson <davemoses3@gmail.com>
2022-04-12 10:01:19 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
     return (
       <div>
         <Navbar />
-        <ScrollToTop />
         <main>
           <Routes>
             <Route path="/*" element={<Home />}></Route>
